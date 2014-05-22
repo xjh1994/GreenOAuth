817a4e1a59fa148af0e457f8acd4fd3d4b95fcc4
zts1993 <zts1993@gmail.com>
2014-05-23 01:19:27 +0800

    initial3

---
+++ src/com/zts1993/bean/GreenOptions.java
@@ -0,0 +1,67 @@
+package com.zts1993.bean;
+
+/**
+ * Created by TianShuo on 14-5-23.
+ */
+public class GreenOptions {
+    private long optionId;
+    private String optionName;
+    private String optionValue;
+    private String autoload;
+
+    public long getOptionId() {
+        return optionId;
+    }
+
+    public void setOptionId(long optionId) {
+        this.optionId = optionId;
+    }
+
+    public String getOptionName() {
+        return optionName;
+    }
+
+    public void setOptionName(String optionName) {
+        this.optionName = optionName;
+    }
+
+    public String getOptionValue() {
+        return optionValue;
+    }
+
+    public void setOptionValue(String optionValue) {
+        this.optionValue = optionValue;
+    }
+
+    public String getAutoload() {
+        return autoload;
+    }
+
+    public void setAutoload(String autoload) {
+        this.autoload = autoload;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GreenOptions that = (GreenOptions) o;
+
+        if (optionId != that.optionId) return false;
+        if (autoload != null ? !autoload.equals(that.autoload) : that.autoload != null) return false;
+        if (optionName != null ? !optionName.equals(that.optionName) : that.optionName != null) return false;
+        if (optionValue != null ? !optionValue.equals(that.optionValue) : that.optionValue != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (optionId ^ (optionId >>> 32));
+        result = 31 * result + (optionName != null ? optionName.hashCode() : 0);
+        result = 31 * result + (optionValue != null ? optionValue.hashCode() : 0);
+        result = 31 * result + (autoload != null ? autoload.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/com/zts1993/bean/GreenUser.java
@@ -0,0 +1,159 @@
+package com.zts1993.bean;
+
+import java.sql.Timestamp;
+
+/**
+ * Created by TianShuo on 14-5-23.
+ */
+public class GreenUser {
+    private long userId;
+    private String userLogin;
+    private String userPass;
+    private String userNicename;
+    private String userEmail;
+    private String userUrl;
+    private Timestamp userRegistered;
+    private String userActivationKey;
+    private Integer userStatus;
+    private String userIntro;
+    private short userLevel;
+    private String userSession;
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserLogin() {
+        return userLogin;
+    }
+
+    public void setUserLogin(String userLogin) {
+        this.userLogin = userLogin;
+    }
+
+    public String getUserPass() {
+        return userPass;
+    }
+
+    public void setUserPass(String userPass) {
+        this.userPass = userPass;
+    }
+
+    public String getUserNicename() {
+        return userNicename;
+    }
+
+    public void setUserNicename(String userNicename) {
+        this.userNicename = userNicename;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserUrl() {
+        return userUrl;
+    }
+
+    public void setUserUrl(String userUrl) {
+        this.userUrl = userUrl;
+    }
+
+    public Timestamp getUserRegistered() {
+        return userRegistered;
+    }
+
+    public void setUserRegistered(Timestamp userRegistered) {
+        this.userRegistered = userRegistered;
+    }
+
+    public String getUserActivationKey() {
+        return userActivationKey;
+    }
+
+    public void setUserActivationKey(String userActivationKey) {
+        this.userActivationKey = userActivationKey;
+    }
+
+    public Integer getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(Integer userStatus) {
+        this.userStatus = userStatus;
+    }
+
+    public String getUserIntro() {
+        return userIntro;
+    }
+
+    public void setUserIntro(String userIntro) {
+        this.userIntro = userIntro;
+    }
+
+    public short getUserLevel() {
+        return userLevel;
+    }
+
+    public void setUserLevel(short userLevel) {
+        this.userLevel = userLevel;
+    }
+
+    public String getUserSession() {
+        return userSession;
+    }
+
+    public void setUserSession(String userSession) {
+        this.userSession = userSession;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GreenUser that = (GreenUser) o;
+
+        if (userId != that.userId) return false;
+        if (userLevel != that.userLevel) return false;
+        if (userActivationKey != null ? !userActivationKey.equals(that.userActivationKey) : that.userActivationKey != null)
+            return false;
+        if (userEmail != null ? !userEmail.equals(that.userEmail) : that.userEmail != null) return false;
+        if (userIntro != null ? !userIntro.equals(that.userIntro) : that.userIntro != null) return false;
+        if (userLogin != null ? !userLogin.equals(that.userLogin) : that.userLogin != null) return false;
+        if (userNicename != null ? !userNicename.equals(that.userNicename) : that.userNicename != null) return false;
+        if (userPass != null ? !userPass.equals(that.userPass) : that.userPass != null) return false;
+        if (userRegistered != null ? !userRegistered.equals(that.userRegistered) : that.userRegistered != null)
+            return false;
+        if (userSession != null ? !userSession.equals(that.userSession) : that.userSession != null) return false;
+        if (userStatus != null ? !userStatus.equals(that.userStatus) : that.userStatus != null) return false;
+        if (userUrl != null ? !userUrl.equals(that.userUrl) : that.userUrl != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (userId ^ (userId >>> 32));
+        result = 31 * result + (userLogin != null ? userLogin.hashCode() : 0);
+        result = 31 * result + (userPass != null ? userPass.hashCode() : 0);
+        result = 31 * result + (userNicename != null ? userNicename.hashCode() : 0);
+        result = 31 * result + (userEmail != null ? userEmail.hashCode() : 0);
+        result = 31 * result + (userUrl != null ? userUrl.hashCode() : 0);
+        result = 31 * result + (userRegistered != null ? userRegistered.hashCode() : 0);
+        result = 31 * result + (userActivationKey != null ? userActivationKey.hashCode() : 0);
+        result = 31 * result + (userStatus != null ? userStatus.hashCode() : 0);
+        result = 31 * result + (userIntro != null ? userIntro.hashCode() : 0);
+        result = 31 * result + (int) userLevel;
+        result = 31 * result + (userSession != null ? userSession.hashCode() : 0);
+        return result;
+    }
+}
